e8f158cc70ce37eda94c250f97164d6de397fa8a
Preston Fegley <fegley.preston@gmail.com>
2025-01-16 03:01:25 +0000

    completed lab 4-1

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -4,3 +4,15 @@
 -- 2935
 
 
+-- What I tried first. This works, but not how they wanted it.
+
+-- SELECT SUM(hits)
+-- FROM stats
+-- WHERE player_id = (SELECT id FROM players WHERE first_name = "Barry" AND last_name = "Bonds");
+
+
+SELECT players.first_name, players.last_name, SUM(stats.hits)
+FROM stats 
+INNER JOIN players ON players.id = stats.player_id
+WHERE players.first_name = "Barry" 
+AND players.last_name = "Bonds";
